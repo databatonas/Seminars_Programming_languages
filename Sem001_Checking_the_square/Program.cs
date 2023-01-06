78e1b6616724ffd4540b7876f2668232f6ae83cd
Yuriy <databatonas@gmail.com>
2023-01-06 20:54:33 +0300

    Задача на квадрат числа

---
+++ Sem001_Checking_the_square/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a == b*b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
