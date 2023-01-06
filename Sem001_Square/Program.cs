6ae5cfdffb431f168395849808c5911b17c96181
Yuriy <databatonas@gmail.com>
2023-01-06 21:15:15 +0300

    Задача выдаёт квадрат числа

---
+++ Sem001_Square/Program.cs
@@ -0,0 +1,4 @@
+﻿// Задача: Написать программу, которая на вход принимает число и выдает его квадрат.
+Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(n*n);
