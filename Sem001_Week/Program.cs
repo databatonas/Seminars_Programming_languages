453e75e97daa69f89da59869dad34b8e98b5240b
Yuriy <databatonas@gmail.com>
2023-01-06 21:23:51 +0300

    Задача на дни недели

---
+++ Sem001_Week/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+Console.WriteLine("Введите целое число от 1 до 7 включительно");
+int number = Convert.ToInt32(Console.ReadLine()); // 1 2 3 4 5 6 7
+
+if(number == 1)
+{
+    Console.Write("Monday");
+}
+
+if(number == 2)
+{
+    Console.Write("Tuesday");
+}
+
+if(number == 3)
+{
+    Console.Write("Wednesday");
+}
+
+if(number == 4)
+{
+    Console.Write("Thusday");
+}
+
+if(number == 5)
+{
+    Console.Write("Friday");
+}
+
+if(number == 6)
+{
+    Console.Write("Saturday");
+}
+
+if(number == 7)
+{
+    Console.Write("Sunday");
+}
+
+if(number >7)
+{
+    Console.Write("You entered an incorrect number");
+}
+
+if(number <1)
+{
+    Console.Write("You entered an incorrect number");
+}
