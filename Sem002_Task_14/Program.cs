1e63a124a90ed74c924e15a9c8cd308c5becab4c
Yuriy <databatonas@gmail.com>
2023-01-08 19:18:07 +0300

    Задача на кратность числа 7 и 23 одновременно

---
+++ Sem002_Task_14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 14
+ /*Напишите программу, которая принимает на вход число 
+   и проверяет, кратно ли оно одновременно 7 и 23.*/
+
+Console.WriteLine("Введите число (используйте запятую при введении дробного числа)");
+Console.ForegroundColor = ConsoleColor.Blue;
+double a = Convert.ToDouble(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.White;
+
+if(a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine($"{a} кратно и 7 и 23 одновременно");
+}
+else
+{
+    Console.WriteLine($"{a} не кратно и 7 и 23 одновременно");
+}
